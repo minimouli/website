9f42d54412d23960081608a2b43ad0bfb80ad62f
Adrien Belminksy <belminksy@gmail.com>
2021-12-12 03:05:16 +0100

    feat: add divider component

---
+++ src/components/Divider.tsx
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) Minimouli
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import styles from '../styles/Divider.module.scss'
+
+const Divider = () => {
+    return (
+        <hr className={styles.divider} />
+    )
+}
+
+export default Divider
