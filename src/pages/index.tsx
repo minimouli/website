a5e5689e3b53584dfab9afc57a87a780f7b61c82
Adrien Belminksy <belminksy@gmail.com>
2021-12-12 04:47:24 +0100

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,9 @@
 
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Container from '../components/Container'
+import Footer from '../components/Footer'
+import InstallSnippet from '../components/InstallSnippet'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
@@ -15,10 +18,38 @@ const Home: NextPage = () => {
 
             <Head>
                 <title>Minimouli</title>
-                <link rel="icon" href="favicon.ico" />
+                <meta name="description" content="Get feedback on your projects without waiting for nao" />
+                <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <h1>hello world</h1>
+            <Container>
+                <main className={styles.hero} >
+
+                    <div className={styles.headlines} >
+                        <h1>Get feedback on your projects without waiting for nao</h1>
+                        <h2>Minimouli offers the best toolchain of testing to help developers maintain the quality of their projects.</h2>
+                    </div>
+
+                    <div className={styles.installation} >
+
+                        <div className={styles.snippet} >
+                            <InstallSnippet />
+                        </div>
+
+                        <span className={styles.caption} >
+                            Once installed, just type
+                            <span className={styles.command} >
+                                minimouli run
+                            </span>
+                            in your repository.
+                        </span>
+
+                    </div>
+
+                </main>
+            </Container>
+
+            <Footer />
 
         </div>
     )
