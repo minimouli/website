183d7b363d819db0920ca5c69a9ddd6151094ba3
Adrien Belminksy <belminksy@gmail.com>
2023-01-07 16:07:01 +0100

    feat: add favicons

--- app/head.tsx
+++ app/head.tsx
@@ -10,7 +10,8 @@ const Head = () => (
         <title>Get feedback on your projects without waiting for nao • Minimouli</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta name="description" content="Get feedback on your projects without waiting for nao" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" media="(prefers-color-scheme: light)" />
+        <link rel="icon" href="/favicon-white.ico" media="(prefers-color-scheme: dark)" />
     </>
 )
 
