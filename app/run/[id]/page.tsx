92e13dacbf43bf0c18f308c068f0ba35c566d41c
Adrien Belminksy <belminksy@gmail.com>
2023-01-17 01:02:50 +0100

    feat(app): add run page

---
+++ app/run/[id]/page.tsx
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) Minimouli
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { Client } from '@minimouli/sdk'
+import { notFound } from 'next/navigation'
+import styles from './Run.module.scss'
+import { Footer } from '../../../components/Footer'
+import { RunHeader } from '../../../components/RunHeader'
+import { RunInformationCard } from '../../../components/cards/RunInformationCard'
+import { RunOverviewCard } from '../../../components/cards/RunOverviewCard'
+import { SuiteCard } from '../../../components/cards/SuiteCard'
+import { Container } from '../../../components/layouts/Container'
+import { fromSuites, sort } from '../../../helpers/light-suite.helper'
+import type { ProjectEntity, RunEntity } from '@minimouli/sdk'
+import type { SuiteSynthesis } from '@minimouli/types/dist/syntheses'
+
+interface RunProps {
+    params: {
+        id: string
+    }
+}
+
+const Run = async ({ params }: RunProps) => {
+
+    const client = new Client()
+
+    let run: RunEntity
+    let project: ProjectEntity
+
+    try {
+        run = await client.runs.get(params.id)
+        const moulinette = await client.moulinettes.get(run.moulinette.id)
+        project = await client.projects.get(moulinette.project?.id ?? '')
+    } catch {
+        notFound()
+    }
+
+    const organizationName = project.organization?.displayName ?? ''
+
+    // @ts-expect-error Bad types in @minimouli/sdk
+    const suites = sort(fromSuites(run.suites as SuiteSynthesis[]))
+        .filter((suite) => suite.tests.length > 0)
+
+    return (
+        <div className={styles.page} >
+            <RunHeader
+                title={project.displayName}
+                caption={[organizationName, project.cycle].join(' / ')}
+            />
+
+            <main className={styles.main} >
+                <Container>
+                    <div className={styles.container} >
+
+                        <div className={styles.suites} >
+                            <div className={styles.result} >
+                                {suites.map((suite) => (
+                                    <SuiteCard key={suite.path.join('/')} suite={suite} />
+                                ))}
+                            </div>
+
+                            <div className={styles.general} >
+                                <RunOverviewCard suites={suites} />
+                                <RunInformationCard run={run} />
+                            </div>
+                        </div>
+
+                    </div>
+                </Container>
+            </main>
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Run
