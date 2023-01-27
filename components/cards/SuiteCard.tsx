81b02f74184a92a9570405ae848420278bc533f4
Adrien Belminksy <belminksy@gmail.com>
2023-01-27 15:42:09 +0100

    feat(components): add category chips

--- components/cards/SuiteCard.tsx
+++ components/cards/SuiteCard.tsx
@@ -6,7 +6,9 @@
  */
 
 import { TestStatus } from '@minimouli/types/syntheses'
+import { HintCategory } from '@minimouli/types/hints'
 import styles from './SuiteCard.module.scss'
+import { CategoryChip } from '../CategoryChip'
 import { LargeIndicator } from '../LargeIndicator'
 import { Test } from '../Test'
 import type { LightSuiteSynthesis } from '../../types/LightSuiteSynthesis'
@@ -32,24 +34,55 @@ const isDividerNeeded = (current: TestSynthesis, tests: TestSynthesis[], index: 
     return next.status === TestStatus.Failure
 }
 
-const SuiteCard = ({ suite }: SuiteCardProps) => (
-    <section className={styles.card} >
-        <div className={styles.head} >
-            <span>{suite.path.join(' / ')}</span>
-            <LargeIndicator value={suite.score} />
-        </div>
+const SuiteCard = ({ suite }: SuiteCardProps) => {
 
-        <div className={styles.body} >
-            {suite.tests.map((test, index) => (
-                <Test
-                    key={test.name}
-                    test={test}
-                    withDivider={isDividerNeeded(test, suite.tests, index)}
-                />
-            ))}
-        </div>
-    </section>
-)
+    const outputCount = suite.tests.filter((test) => test.hint?.category === HintCategory.Output).length
+    const exitCodeCount = suite.tests.filter((test) => test.hint?.category === HintCategory.ExitCode).length
+    const timeoutCount = suite.tests.filter((test) => test.hint?.category === HintCategory.Timeout).length
+
+    return (
+        <section className={styles.card} >
+            <div className={styles.head} >
+                <div className={styles.title} >
+                    <span className={styles.name} >{suite.path.join(' / ')}</span>
+                    <div className={styles.chips} >
+                        {outputCount > 0 && (
+                            <div className={styles.chip} >
+                                <CategoryChip category={HintCategory.Output} />
+                                <span>{outputCount} output{outputCount === 1 ? '' : 's'}</span>
+                            </div>
+                        )}
+
+                        {exitCodeCount > 0 && (
+                            <div className={styles.chip} >
+                                <CategoryChip category={HintCategory.ExitCode} />
+                                <span>{exitCodeCount} exit code{exitCodeCount === 1 ? '' : 's'}</span>
+                            </div>
+                        )}
+
+                        {timeoutCount > 0 && (
+                            <div className={styles.chip} >
+                                <CategoryChip category={HintCategory.Timeout} />
+                                <span>{timeoutCount} timeout{timeoutCount === 1 ? '' : 's'}</span>
+                            </div>
+                        )}
+                    </div>
+                </div>
+                <LargeIndicator value={suite.score} />
+            </div>
+
+            <div className={styles.body} >
+                {suite.tests.map((test, index) => (
+                    <Test
+                        key={test.name}
+                        test={test}
+                        withDivider={isDividerNeeded(test, suite.tests, index)}
+                    />
+                ))}
+            </div>
+        </section>
+    )
+}
 
 export {
     SuiteCard
